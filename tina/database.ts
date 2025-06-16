86153cb6e00b6126f03f3b56e3099901474198b1
@lpaz <consultor.011@smarttmt.com>
2025-06-16 10:51:54 -0500

    Version se adiciona la logica par el despligue local

--- tina/database.ts
+++ tina/database.ts
@@ -11,18 +11,18 @@ export default (async () => {
         module?.default?.MongodbLevel ||
         module?.default;
 
-    return createDatabase({
-        gitProvider: new GitHubProvider({
-            repo: process.env.GITHUB_REPO!,
-            owner: process.env.GITHUB_OWNER!,
-           // token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
-            branch: process.env.GITHUB_BRANCH || "main"
-        }),
-        databaseAdapter: new MongodbLevel({
-            collectionName: "tinacms",
-            dbName: "tinacms",
-            mongoUri: 'mongodb://root:tina-startlight@172.17.0.1:27017',
-        }),
-        useLocalClient: true // Necesario para self-hosted
-    })
+    // return createDatabase({
+    //     gitProvider: new GitHubProvider({
+    //         repo: process.env.GITHUB_REPO!,
+    //         owner: process.env.GITHUB_OWNER!,
+    //        // token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
+    //         branch: process.env.GITHUB_BRANCH || "main"
+    //     }),
+    //     databaseAdapter: new MongodbLevel({
+    //         collectionName: "tinacms",
+    //         dbName: "tinacms",
+    //         mongoUri: 'mongodb://root:tina-startlight@172.17.0.1:27017',
+    //     }),
+    //     useLocalClient: true // Necesario para self-hosted
+    // })
 })()
